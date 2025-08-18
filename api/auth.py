4dc15224949a5bf08fb573f8f7c7a896d493e6b1
Lohit P Talavar <128409310+lohitpt252003@users.noreply.github.com>
2025-08-18 23:06:10 +0530

    added token in auth.py

--- api/auth.py
+++ api/auth.py
@@ -1,8 +1,13 @@
 from flask import Blueprint, jsonify
 from services.github_services import get_file
 import json
+import sys, os
 
 auth_bp = Blueprint('auth_bp', __name__)
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from utils.jwt_token import generate_token
 
 @auth_bp.route('/user/<user_id>', methods=['GET'])
 def get_user(user_id):
@@ -25,6 +30,9 @@ def get_user(user_id):
             "user_id": user_data.get("user_id")
         }
         
-        return jsonify(filtered_data)
+        # Generate a token
+        token = generate_token(filtered_data)
+        
+        return jsonify({"user": filtered_data, "token": token})
     except json.JSONDecodeError:
         return jsonify({"error": "Failed to decode user data."}), 500
