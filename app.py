6ddf023e75d6e7bc137186f64389d645603e2fc8
Lohit P Talavar <128409310+lohitpt252003@users.noreply.github.com>
2025-08-24 03:08:02 +0530

    re init

--- app.py
+++ app.py
@@ -3,24 +3,15 @@ from flask_cors import CORS
 from dotenv import load_dotenv
 import os
 
-# from api.users import users_bp
+
 from api.problems_api import problems_bp
-from api.submissions_api import submissions_bp
-# from api.contests_api import contests_bp
-# from api.admin import admin_bp
-from api.auth import auth_bp
 
 
 def create_app():
     app = Flask(__name__)
     CORS(app)
 
-    # app.register_blueprint(users_bp, url_prefix='/api/users')
     app.register_blueprint(problems_bp, url_prefix='/api/problems')
-    app.register_blueprint(submissions_bp, url_prefix='/api/submissions')
-    # app.register_blueprint(contests_bp, url_prefix='/api/contests')
-    # app.register_blueprint(admin_bp, url_prefix='/api/admin')
-    app.register_blueprint(auth_bp, url_prefix='/api/auth')
 
     @app.route("/")
     def root():
